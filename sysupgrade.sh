5f0897aec8efd6e4f6c81a7eac35a0e306f0e95e
yjw8886 <yjw8886@gmail.com>
2023-04-04 23:19:10 +0800

    优化代码

--- sysupgrade.sh
+++ sysupgrade.sh
@@ -8,7 +8,7 @@ ROUTER_IP="10.1.1.1"
 # 软件仓库地址
 REPO_URL="https://github.com/yjw8336/lede"
 # 当前日期
-CURRENT_DATE=$(date +%Y-%m-%d)
+CURRENT_DATE=$(date +%Y-%m%d%H%M%S)
 
 
 # 检查是否有互联网连接
@@ -32,38 +32,42 @@ latest_release=${release_url##*/}
 
 # 下载固件
 echo "正在下载固件 -->${REPO_URL}/releases/download/${latest_release}/openwrt-rockchip-armv8-friendlyarm_nanopi-r2s-ext4-sysupgrade.img.gz"
-wget "${REPO_URL}/releases/download/${latest_release}/openwrt-rockchip-armv8-friendlyarm_nanopi-r2s-ext4-sysupgrade.img.gz" || exit 1
+# https://github.com/yjw8336/lede/releases/download/20230404-a8596c6/openwrt-rockchip-armv8-friendlyarm_nanopi-r2s-ext4-sysupgrade.img.gz
+#
+wget "${REPO_URL}/releases/download/${latest_release}/openwrt-rockchip-armv8-friendlyarm_nanopi-r2s-ext4-sysupgrade.img.gz" -O ${CURRENT_DATE}.sysupgrade.img.gz  || exit 1
 
 # 下载sha256sums
 echo "正在下载sha256sums -->${REPO_URL}/releases/download/${latest_release}/sha256sums"
-wget "${REPO_URL}/releases/download/${latest_release}/sha256sums" || exit 1
+wget "${REPO_URL}/releases/download/${latest_release}/sha256sums" -O ${CURRENT_DATE}.sha256sums || exit 1
 
 
 # 验证固件
-checksum=$(shasum -a 256 openwrt-rockchip-armv8-friendlyarm_nanopi-r2s-ext4-sysupgrade.img.gz)
-orSum=$(cat sha256sums | grep openwrt-rockchip-armv8-friendlyarm_nanopi-r2s-ext4-sysupgrade.img.gz)
+checksum=$(shasum -a 256 ${CURRENT_DATE}.sysupgrade.img.gz | awk '{print $1}' )
+orSum=$(cat ${CURRENT_DATE}.sha256sums | grep openwrt-rockchip-armv8-friendlyarm_nanopi-r2s-ext4-sysupgrade.img.gz | awk '{print $1}' )
+
 if [ "$checksum" != "$orSum" ]; then
     echo "checksum error"
     echo "文件校验失败，请检查网络后重试。"
+    echo "checksum: $checksum"
+    echo "orSum: $orSum"
     exit 1
 fi
  echo "文件校验成功"
 
 #备份当前配置信息
 echo "正在备份当前配置信息"
-ssh root@${ROUTER_IP} "sysupgrade -b /tmp/tmp/${CURRENT_DATE}.tar.gz"
+ssh root@${ROUTER_IP} "sysupgrade -b /tmp/tmp/${CURRENT_DATE}.bak.tar.gz"
 
 #下载备份文件
 echo "正在下载备份文件"
-scp -r root@${ROUTER_IP}:/tmp/tmp/${CURRENT_DATE}.tar.gz ~/Downloads/update
-
-
-exit 0
+scp -r root@${ROUTER_IP}:/tmp/tmp/${CURRENT_DATE}.bak.tar.gz ~/Downloads/update
 
 
 # 上传固件
 echo "正在上传固件"
-scp -r ~/Downloads/update/openwrt-rockchip-armv8-friendlyarm_nanopi-r2s-ext4-sysupgrade.img.gz root@${ROUTER_IP}:/tmp/tmp/
+scp -r ~/Downloads/update/${CURRENT_DATE}.sysupgrade.img.gz root@${ROUTER_IP}:/tmp/tmp/
 # 执行更新
 echo "正在执行更新,请耐心等待"
-ssh root@${ROUTER_IP} "sysupgrade -v /tmp/tmp/openwrt-rockchip-armv8-friendlyarm_nanopi-r2s-ext4-sysupgrade.img.gz"
+ssh root@${ROUTER_IP} "sysupgrade  /tmp/tmp/${CURRENT_DATE}.sysupgrade.img.gz"
+
+
